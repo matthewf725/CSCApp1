a96c6c80e311ddc17d1057596898f77ed4deef71
Matthew <matthewf725@gmail.com>
2025-10-07 11:13:31 -0700

    added fetchUsers() and useEffect to load users from backend

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,29 +1,40 @@
-// src/MyApp.jsx
-import React, { useState } from "react";
+// packages/react-frontend/src/MyApp.jsx
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
-
 function MyApp() {
-    const [characters, setCharacters] = useState([]);
-    function updateList(person) {
-        setCharacters([...characters, person]);
-    }
-    function removeOneCharacter(index) {
-        const updated = characters.filter((character, i) => {
-        return i !== index;
-        });
-        setCharacters(updated);
-    }
-    return (
+  const [characters, setCharacters] = useState([]);
+
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  function updateList(person) {
+    setCharacters([...characters, person]);
+  }
+
+  function removeOneCharacter(index) {
+    const updated = characters.filter((_, i) => i !== index);
+    setCharacters(updated);
+  }
+
+  return (
     <div className="container">
-        <Table
-        characterData={characters}
-        removeCharacter={removeOneCharacter}
-        />
-        <Form handleSubmit={updateList} />
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
     </div>
-    );
+  );
 }
 
 export default MyApp;
